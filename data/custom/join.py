8151914f9aa997f90005a91b17185f6b9b0e60f2
Mallika <mallika2011@yahoo.com>
2020-10-20 00:59:21 +0530

    Fix dataframe join bug & update combined feature files

--- data/custom/join.py
+++ data/custom/join.py
@@ -13,9 +13,29 @@ musicaldf = pd.read_csv(musicalf)
 extraf = './extra-features.csv'
 extradf = pd.read_csv(extraf)
 
-musicaldf.sort_index(inplace=True)
-extradf.sort_index(inplace=True)
+musicaldf.sort_values('id',inplace=True)
+musicaldf.set_index('id', inplace=True)
 
-musicaldf.loc[musicaldf.id.isin(extradf.id), ['name', 'artists','popularity','year']] = extradf[['name', 'artists','popularity','year']]
+extradf.sort_values('id',inplace=True)
+extradf.set_index('id', inplace=True)
 
-musicaldf.to_csv('./combined-features.csv') 
+musicaldf.loc[musicaldf.index.isin(extradf.index), ['name', 'artists','popularity','year']] = extradf[['name', 'artists','popularity','year']]
+musicaldf.reset_index(inplace=True)
+
+
+#get rid of null songs
+null_files = ['null1e','null2e','null3e','null4e']
+null_ids = []
+
+for filename in null_files : 
+    with open('./subfiles/'+filename+'.txt') as f:
+        content = f.readlines()
+    null_ids += [x.strip() for x in content]
+
+
+# print(len(null_ids))
+# print(null_ids)
+musicaldf = musicaldf[~musicaldf['id'].isin(null_ids)]
+
+
+musicaldf.to_csv('./combined-features-new.csv') 
