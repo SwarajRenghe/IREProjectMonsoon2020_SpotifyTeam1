fb16ad4852e53f4028a55ff40967d65312fc24b2
Mallika <mallika2011@yahoo.com>
2020-10-28 13:16:57 +0530

    Modify search to account for new songs

--- searching/top_k.py
+++ searching/top_k.py
@@ -14,19 +14,77 @@ import time
 import os
 import sys
 import time 
+import requests 
+
+import pickle
+from pickle import load
 
 
 TOPK = 10
-directory = sys.argv[1]
-# query = sys.argv[2].split(':')
-index_dir = sys.argv[3]
 
-query_id = sys.argv[2]
-# query_features = np.array(query[1].split(" ")).astype(np.float)
+try:
+    directory = sys.argv[1]
+    index_dir = sys.argv[3]
+    query_id = sys.argv[2]
+except:
+    print("Script Usage:")
+    print("python3 top_k.py <path to embeddings directory> <query song ID> <ID2Songs directory> <Auth token (optional)>")
+    exit(0)
 
 #load the id2name dataframe and prepare it
 id2name = pd.read_csv(index_dir+'id2song.csv',sep=':')
 song_name = id2name.set_index('id')['name'].to_dict()
+features = ["acousticness","danceability","duration_ms","energy","instrumentalness","key","liveness",
+            "loudness","mode","popularity","speechiness","tempo","valence","year"]
+
+#load and prepare the regression model 
+scaler = load(open('../approach_A/training/scaler.pkl', 'rb'))
+filename='../approach_A/training/MLPRegressor_model.sav'
+mlpreg_model =pickle.load(open(filename, 'rb'))
+
+
+def get_from_api():
+
+    final = {}
+
+    #API endpoint 
+    URL1 = "https://api.spotify.com/v1/audio-features/"
+    URL2 = "https://api.spotify.com/v1/tracks/"
+    try:
+        TOKEN = sys.argv[4]
+    except:
+        print("Please pass an authorization token")
+        exit(0)
+    
+    r = requests.get(url = URL1+query_id,  headers={'Authorization': 'Bearer '+TOKEN})
+    data1 = r.json()
+    r = requests.get(url = URL2+query_id,  headers={'Authorization': 'Bearer '+TOKEN})
+    data2 = r.json()
+
+    if (('error' in data1) and (data1['error']['status']==401)) or (('error' in data2) and (data2['error']['status']==401)) :
+        print("Token expired :( ")
+        exit(0)
+    elif ('error' in data1) or ('error' in data2):
+        print("\nSorry, spotify doesn't seem to have this song you are looking for :/")
+        exit(0)
+
+    print("Data retrieved\n")
+
+    final['year'] = data2['album']['release_date']
+    final['popularity'] = data2['popularity']
+    song_name[query_id] = data2['name']
+
+
+    for key in data1:
+        if key in features:
+            final[key] = data1[key]
+
+    final = sorted(final.items(), key=lambda x: x[0])
+    final = np.array([float(i[1]) for i in final]).reshape(1, -1)
+
+    model_result=scaler.inverse_transform(mlpreg_model.predict(final))
+
+    return model_result
 
 def compute_emb(filename):
     # print("emb", filename)
@@ -82,6 +140,11 @@ pool0 = multiprocessing.Pool()
 files = [filename for filename in os.listdir(directory)]   
 outputs0 = pool0.map(compute_emb, files)
 query_features = [item for sublist in outputs0 for item in sublist]
+
+if ("".join(map(str, query_features)) == ""):
+    print("Getting spotify features ...")
+    query_features = get_from_api()
+
 
 #Perform multithreaded search for the songs
 pool = multiprocessing.Pool() 
